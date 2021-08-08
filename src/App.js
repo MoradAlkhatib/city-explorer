5cc17a577948fc2f5964585aff01a350bfd7070d
moradalkhatib <muradalkhateb@icloud.com>
2021-08-08 20:44:10 +0300

    ed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import React, { Component } from 'react'
 import City from './component/City'
 import "bootstrap/dist/css/bootstrap.min.css";
+import Errorx from './component/Errorx';
 class App extends Component {
+   cityError=(error)=>{
+    
+   console.log(error);
+  }
+  
   render() {
     return (
       <>
-        <City />
+        <City getError={this.cityError}/>
+        <Errorx />
       </>
     )
   }
