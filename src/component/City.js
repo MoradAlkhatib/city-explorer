32fff387bbd52d2bb9a5218a75fe1200a1549c3c
Murad Alkhatib <84707872+MoradAlkhatib@users.noreply.github.com>
2021-08-09 15:27:41 +0300

    Update City.js

--- src/component/City.js
+++ src/component/City.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Form, Button, Card ,ListGroup ,ListGroupItem} from "react-bootstrap";
+import { Form, Button, Card, ListGroup, ListGroupItem } from "react-bootstrap";
+import PrintError from "./PrintError";
 
 class City extends Component {
   constructor(props) {
@@ -10,8 +11,7 @@ class City extends Component {
       lat: "0.0",
       long: "0.0",
       showData: false,
-      img:"",
-      error:""
+      error: "",
     };
   }
 
@@ -21,74 +21,75 @@ class City extends Component {
       showData: false,
     });
   };
+
   submitHandler = async (e) => {
     e.preventDefault();
 
-    try{
-      axios.get(`https://eu1.locationiq.com/v1/search.php?key=pk.f7162d9d9da6a0d03a35e52f15d974c8&q=${this.state.cityName}&format=json`)
-      .then((response) => {
-          let data = response.data[0];
-      axios.get(`https://maps.locationiq.com/v3/staticmap?key=pk.f7162d9d9da6a0d03a35e52f15d974c8&center=${this.state.lat},${this.state.long}&size=600x600&zoom=14&path=fillcolor:%2390EE90|weight:2|color:blue|enc:}woiBkrk}Mb@iKtC\`CEhBsD|C`)
-      .then(resp=>{
-            console.log(resp.config.url);
-       this.setState({
+    try {
+      let cityLocation = await axios.get(
+        `https://eu1.locationiq.com/v1/search.php?key=pk.f7162d9d9da6a0d03a35e52f15d974c8&q=${this.state.cityName}&format=json`
+      );
+      let data = await cityLocation.data[0];
+      this.setState({
         showData: true,
         cityName: data.display_name,
         lat: data.lat,
         long: data.lon,
-        img:resp.config.url
       });
-
-      })
-
-
-        });
-
-    }
-    catch(e){
+    } catch (err) {
       this.setState({
-        error:e,
-      })
+        error: "Error : you use some thing not valid",
+      });
     }
-    this.props.getError(this.state.error);
-
-
   };
+
   render() {
     return (
       <div className="container">
-        <Form onSubmit={(e) => this.submitHandler(e)} style={{display:"flex"}}>
-          <Form.Control style={{width:"30%"}}
+        <Form
+          onSubmit={(e) => this.submitHandler(e)}
+          style={{ display: "flex" }}
+        >
+          <Form.Control
+            style={{ width: "30%" }}
             type="text"
             placeholder="City"
             onChange={(e) => this.handlerCity(e)}
           />
 
-          <Button variant="primary" type="submit" size="sm" style={{marginLeft:"10px"}}>
+          <Button
+            variant="primary"
+            type="submit"
+            size="sm"
+            style={{ marginLeft: "10px" }}
+          >
             explore
           </Button>
         </Form>
+        {!this.state.showData && (
+          <h1 style={{ fontSize: "30px", color: "red" }}>{this.state.error}</h1>
+        )}
         {this.state.showData && (
-           <Card style={{ width: '18rem' ,marginTop:"20px"}}>
-           <Card.Img variant="top" src={this.state.img} />
+          <Card style={{ width: "18rem", marginTop: "20px" }}>
+            <Card.Img
+              variant="top"
+              src={`https://maps.locationiq.com/v3/staticmap?key=pk.f7162d9d9da6a0d03a35e52f15d974c8&center=${this.state.lat},${this.state.long}&size=600x600&zoom=14&path=fillcolor:%2390EE90|weight:2|color:blue|enc:}woiBkrk}Mb@iKtC\`CEhBsD|C`}
+            />
 
-           <Card.Body>
-             <Card.Title>{this.state.cityName}</Card.Title>
-
-           </Card.Body>
-           <ListGroup className="list-group-flush">
-             <ListGroupItem>lat: {this.state.lat}</ListGroupItem>
-             <ListGroupItem>lon: {this.state.long}</ListGroupItem>
-
-           </ListGroup>
-           {/* <Card.Body>
+            <Card.Body>
+              <Card.Title>{this.state.cityName}</Card.Title>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>lat: {this.state.lat}</ListGroupItem>
+              <ListGroupItem>lon: {this.state.long}</ListGroupItem>
+            </ListGroup>
+            {/* <Card.Body>
 
            </Card.Body> */}
-
-         </Card>
-
-
+          </Card>
         )}
+
+        <PrintError />
       </div>
     );
   }
