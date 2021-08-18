bad8ba3633f976df4b4683032f0d59e2a5caec2e
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-08-18 16:46:15 +0300

    a

---
+++ src/component/Movie.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Carousel } from "react-bootstrap";
+import movieImage from "../image/movie.png";
+class Movie extends Component {
+  render() {
+    return (
+      <div>
+        <>
+          <h1 className="headerTextH1">Movies Of {this.props.nameA}</h1>
+          <Carousel className="containerMovie">
+            {this.props.movieData.map((item) => {
+              console.log(typeof item.imgUrl);
+              return (
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src={item.imgUrl.includes("jpg") ? item.imgUrl : movieImage}
+                    alt="First slide"
+                  />
+                  <Carousel.Caption className="textCarousel">
+                    <h3>{item.title}</h3>
+                    <p>{item.overview}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              );
+            })}
+          </Carousel>
+        </>
+      </div>
+    );
+  }
+}
+
+export default Movie;
--- src/component/Movies.js
+++ src/component/Movies.js
@@ -1,32 +1,16 @@
-import React, { Component } from "react";
-import { Carousel } from "react-bootstrap";
-import movieImage from "../image/movie.png";
+import React, { Component } from 'react'
+import Movie from './Movie'
 export class Movies extends Component {
   render() {
     return (
-      <>
-    <h1 className='headerTextH1'>Movies Of {this.props.nameA}</h1>
-      <Carousel className="containerMovie">
-        {this.props.movieData.map((item) => {
-          console.log(typeof item.imgUrl);
-          return (
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src={item.imgUrl.includes("jpg") ? item.imgUrl : movieImage}
-                alt="First slide"
-              />
-              <Carousel.Caption className="textCarousel">
-                <h3>{item.title}</h3>
-                <p>{item.overview}</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          );
-        })}
-      </Carousel>
-      </>
-    );
+      <div>
+        <Movie movieData={this.props.movieData} nameA={this.props.cityName}/>
+      </div>
+    )
   }
+
 }
 
-export default Movies;
+export default Movies
+
+
